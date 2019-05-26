3c110c18cce319254dba7a741ca7135c5d7c99e3
Monica Iqbal <monica.iqbal@mail.utoronto.ca>
2019-05-26 01:51:21 -0400

    Updates

---
+++ precache-manifest.b75a588bc1a0d42a20dff1e93ca1b489.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b0b54f334811afddf2d990ed94e457d",
+    "url": "/website-new/index.html"
+  },
+  {
+    "revision": "287f59aee2b7c94fb10b",
+    "url": "/website-new/static/css/2.89666679.chunk.css"
+  },
+  {
+    "revision": "0e3fb8c081fef05c7b7c",
+    "url": "/website-new/static/css/main.53e2fbca.chunk.css"
+  },
+  {
+    "revision": "287f59aee2b7c94fb10b",
+    "url": "/website-new/static/js/2.e7f94194.chunk.js"
+  },
+  {
+    "revision": "0e3fb8c081fef05c7b7c",
+    "url": "/website-new/static/js/main.742de66a.chunk.js"
+  },
+  {
+    "revision": "45a78cabad5ea76779e1",
+    "url": "/website-new/static/js/runtime~main.fa13c6ed.js"
+  },
+  {
+    "revision": "05ebe0a9640b617cbe954eb43e701a24",
+    "url": "/website-new/static/media/IMG_0081.05ebe0a9.jpg"
+  },
+  {
+    "revision": "ea65e0dd8c6be70090024d3fcaace818",
+    "url": "/website-new/static/media/IMG_0097.ea65e0dd.jpg"
+  },
+  {
+    "revision": "95b934282fc588205041f9d3601c164b",
+    "url": "/website-new/static/media/IMG_0357.95b93428.jpg"
+  },
+  {
+    "revision": "3dfae38114dd45378b976c6217043a8e",
+    "url": "/website-new/static/media/IMG_0358.3dfae381.jpg"
+  },
+  {
+    "revision": "e65b7cfdaabcb328259d7ed0d2fccd44",
+    "url": "/website-new/static/media/IMG_0410.e65b7cfd.jpg"
+  },
+  {
+    "revision": "20c8ad40395594c899114106c5d5c950",
+    "url": "/website-new/static/media/IMG_0922.20c8ad40.jpeg"
+  },
+  {
+    "revision": "055142986028b0c8dc47961dc736f3c6",
+    "url": "/website-new/static/media/IMG_1202.05514298.jpg"
+  },
+  {
+    "revision": "22ad6a7de370ea1bdb3d8cee37536b68",
+    "url": "/website-new/static/media/IMG_1250.22ad6a7d.jpg"
+  },
+  {
+    "revision": "25e916a0ac45287824a36d63ba3f4394",
+    "url": "/website-new/static/media/IMG_1433.25e916a0.jpg"
+  },
+  {
+    "revision": "1d034b25ad5394014ec9783cd6c3b735",
+    "url": "/website-new/static/media/IMG_1458.1d034b25.jpg"
+  },
+  {
+    "revision": "1a9f20880ea00eeaee1e323758e62334",
+    "url": "/website-new/static/media/IMG_5479.1a9f2088.jpeg"
+  },
+  {
+    "revision": "029ee1f854797243f9626183a082fb82",
+    "url": "/website-new/static/media/IMG_6664.029ee1f8.jpeg"
+  },
+  {
+    "revision": "da0a466dfe0558fd79b02d5002155fc4",
+    "url": "/website-new/static/media/IMG_7220.da0a466d.jpeg"
+  },
+  {
+    "revision": "259c1bdf773b32aacc3f1bc09f7c4fd9",
+    "url": "/website-new/static/media/IMG_8397.259c1bdf.jpeg"
+  },
+  {
+    "revision": "738b35357229db4c31584a86bdbf13d9",
+    "url": "/website-new/static/media/IMG_8398.738b3535.jpeg"
+  },
+  {
+    "revision": "b2deda44a58b47d6c5ffc9eeeafb4027",
+    "url": "/website-new/static/media/IMG_8511.b2deda44.jpeg"
+  },
+  {
+    "revision": "9545c208477e433cd94c92863f148718",
+    "url": "/website-new/static/media/monica.9545c208.jpg"
+  },
+  {
+    "revision": "0b14e9c2e49de885959913eaca8a1fb5",
+    "url": "/website-new/static/media/onaroll.0b14e9c2.png"
+  },
+  {
+    "revision": "7b624661b4a994a116b740c31ffa3aac",
+    "url": "/website-new/static/media/spaceshooter.7b624661.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81f4aaeca9b737470035f0535b60a441.js"
+  "/website-new/precache-manifest.b75a588bc1a0d42a20dff1e93ca1b489.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website-new/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
